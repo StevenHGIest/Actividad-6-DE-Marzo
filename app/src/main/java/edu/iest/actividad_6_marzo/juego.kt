8f15a7debacdaa3f4ae138f320efe85240723be3
StevenHGIest <steven.horstman@iest.edu.mx>
2023-03-06 18:32:54 -0600

    Vistas terminadas

---
+++ app/src/main/java/edu/iest/actividad_6_marzo/juego.kt
@@ -0,0 +1,11 @@
+package edu.iest.actividad_6_marzo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class juego : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_juego)
+    }
+}
